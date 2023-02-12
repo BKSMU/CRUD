f07aaf8533b523bbc55743d75287f32d5e2fcb8e
vtr03 <vtr03@192.168.12.112>
2023-02-12 19:42:52 +0900

    validationとpom辺りの見直しが必要

---
+++ src/main/java/com/example/CRUD/CRUDForm.java
@@ -0,0 +1,64 @@
+package com.example.CRUD;
+
+import jakarta.validation.constraints.NotNull;
+
+public class CRUDForm {
+	
+	private int code;
+
+	// 商品名
+	@NotEmpty
+	private String name;
+	
+	// 金額
+	@NotNull
+	private int unitPrice;
+
+	// 数量
+	@NotNull
+	private int count;
+	
+	// おすすめ商品フラグ
+	@NotNull
+	private int IsPr;
+
+	public String getName() {
+		return name;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+
+	public void setUnitPrice(int unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getIsPr() {
+		return IsPr;
+	}
+
+	public void setIsPr(int isPr) {
+		IsPr = isPr;
+	}
+}
